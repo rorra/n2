2feff198e6c29ffd1169745aa105f409ab190375
Russell Branca <chewbranca@gmail.com>
2011-08-12 11:31:42 -0700

    Stop spamming non configured twitter error msgs

--- app/workers/twitter_scheduled_worker.rb
+++ app/workers/twitter_scheduled_worker.rb
@@ -2,7 +2,11 @@ class TwitterScheduledWorker
   @queue = :twitter_scheduled
 
   def self.perform
-    Newscloud::Tweeter.new.tweet_hot_items
+    begin
+      Newscloud::Tweeter.new.tweet_hot_items
+    rescue Newscloud::TweeterDisabled => e
+      Rails.logger.info("ERROR:: TwitterScheduledWorker failed with: #{e.inspect}")
+    end
   end
 
 end
