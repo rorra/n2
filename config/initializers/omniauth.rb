d7144321b18257e99954e8dacc576c8eb436fba2
Russell Branca <chewbranca@gmail.com>
2011-09-28 15:38:04 -0700

    Auth updates and misc changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,25 @@
-ActionController::Dispatcher.middleware.use OmniAuth::Builder do
-  provider :facebook, '189970861891', '6339e953ad2d278db85e3e83914cdd60', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
-  provider :twitter, 'evv6Ua7vFLzvwNrUt41oyg', 'jtKfrHEFZVZ318AFK57GkQRfDLzB4yScG2IDrmyED8'
+# Inspired by:
+# http://blog.hulihanapplications.com/browse/view/69-autodetecting-oauth-providers-with-omniauth
+
+config = YAML::load_file(File.join(Rails.root, "config", "providers.yml"))
+APP_CONFIG['omniauth'] = config
+
+#take keys of hash and transform those to a symbols
+# HACK: need this for omniauth, explodes occur with string keys for client_options
+def transform_keys_to_symbols(value)
+  return value if not value.is_a?(Hash)
+  hash = value.inject({}){|memo,(k,v)| memo[k.to_sym] = transform_keys_to_symbols(v); memo}
+  return hash
+end
+
+if config.present?
+  ActionController::Dispatcher.middleware.use OmniAuth::Builder do
+    config["providers"].each do |name, settings|
+      options = settings["options"] || {}
+      # HACK: need keys as symbols or facebook auth explodes
+      options = transform_keys_to_symbols(options)
+      provider name.to_sym, settings["key"], settings["secret"], options
+      #provider name.to_sym, credentials["key"], credentials["secret"], {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+    end
+  end
 end
