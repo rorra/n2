43e51b158752cb16f7cfbe544c911b2f3f38d1e3
Russell Branca <chewbranca@gmail.com>
2011-10-03 11:15:03 -0700

    Fixing admin csrf token

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,5 +1,10 @@
 // Place your application-specific jQuery JavaScript functions and classes here
 
+$(document).ajaxSend(function(e, xhr, options) {
+  var token = $("meta[name='csrf-token']").attr("content");
+  xhr.setRequestHeader("X-CSRF-Token", token);
+});
+
 function rebuild_facebook_dom() {
   try {
     FB.XFBML.Host.parseDomTree();
