60a5338943663873d347e5528b8f89a55117be9e
Russell Branca <chewbranca@gmail.com>
2011-10-07 15:48:34 -0700

    Sitemap worker update

--- app/workers/sitemap_worker.rb
+++ app/workers/sitemap_worker.rb
@@ -2,7 +2,7 @@ class SitemapWorker
   @queue = :sitemaps
 
   def self.perform()
-    SitemapGenerator::Sitemap.verbose = verbose
+    SitemapGenerator::Sitemap.verbose = false
     SitemapGenerator::Sitemap.create
     SitemapGenerator::Sitemap.ping_search_engines  
   end
