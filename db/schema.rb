1728bdc7ce2e87110d18b1c46ab22035c01dd79b
Seth <eyelevel@dev.local>
2011-10-25 13:26:12 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -597,6 +597,7 @@ ActiveRecord::Schema.define(:version => 20111012215608) do
     t.boolean  "is_approved",                :default => true
     t.integer  "votes_tally",                :default => 0
     t.integer  "comments_count",             :default => 0
+    t.integer  "questions_count",            :default => 0
     t.boolean  "is_blocked",                 :default => false
     t.boolean  "is_featured",                :default => false
     t.datetime "featured_at"
@@ -629,6 +630,7 @@ ActiveRecord::Schema.define(:version => 20111012215608) do
     t.boolean  "is_approved",              :default => true
     t.integer  "votes_tally",              :default => 0
     t.integer  "comments_count",           :default => 0
+    t.integer  "guesses_count",            :default => 0
     t.boolean  "is_blocked",               :default => false
     t.boolean  "is_featured",              :default => false
     t.datetime "featured_at"
