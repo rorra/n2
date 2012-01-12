b3fe9427db1fd00ab5bad3d005b901569f413d8d
Russell Branca <chewbranca@gmail.com>
2012-01-12 13:09:11 -0800

    Testing out fix for unicorn errors

--- config/initializers/resque_connection_fix.rb
+++ config/initializers/resque_connection_fix.rb
@@ -11,6 +11,7 @@
 # From:
 #   http://coderrr.wordpress.com/2009/01/08/activerecord-threading-issues-and-resolutions/
 
+=begin
 module ActiveRecord::ConnectionAdapters
   class MysqlAdapter
     alias_method :execute_without_retry, :execute
@@ -28,3 +29,4 @@ module ActiveRecord::ConnectionAdapters
     end
   end
 end
+=end
