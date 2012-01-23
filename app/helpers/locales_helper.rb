e030f646ee23a169f3f9936feba1824827cd19d0
Russell Branca <chewbranca@gmail.com>
2012-01-23 13:23:53 -0800

    Updating locales helper for users

--- app/helpers/locales_helper.rb
+++ app/helpers/locales_helper.rb
@@ -1,17 +1,17 @@
 module LocalesHelper
 
   def generic_posted_by item, user = nil, date = nil
-    user ||= item.user
+    user ||= item.item_user
     posted_by item, :user => user
   end
 
   def stories_posted_by item, user = nil, format = nil
-    user ||= item.user
+    user ||= item.item_user
     posted_by item, :user => user, :date => true, :format => nil
   end
 
   def user_posted_item item, user = nil
-    user ||= item.user
+    user ||= item.item_user
     I18n.translate('user_posted_item', :fb_name => local_linked_profile_name(user, :only_path => false, :format => 'html'), :title => link_to(item.item_title, polymorphic_url(item.item_link,:only_path => false) ), :date => timeago(item.created_at)).html_safe
   end
 
