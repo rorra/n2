265211026e500431bfacb524a86ecd0e79043cb9
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-03-02 23:00:45 -0300

    Contents updated on the admin

--- app/controllers/admin/contents_controller.rb
+++ app/controllers/admin/contents_controller.rb
@@ -1,85 +1,74 @@
 class Admin::ContentsController < AdminController
-
   cache_sweeper :story_sweeper, :only => [:create, :update, :destroy]
 
+  before_filter :find_content, :only => [:show, :edit, :update, :destroy]
+
   def index
-    render 'shared/admin/index_page', :layout => 'new_admin', :locals => {
-      :items => Content.paginate(:page => params[:page], :per_page => 20, :order => "created_at desc"),
-      :model => Content,
-      :fields => [:title, :user_id, :score, :comments_count, :is_blocked, :created_at],
-      :associations => { :belongs_to => { :user => :user_id, :source => :source } },
-      :paginate => true
-    }
+    respond_to do |format|
+      format.html do
+        search = {"meta_sort" => "created_at.desc"}.merge(params[:search] || {})
+        @search = Content.search(search)
+        @contents = @search.paginate(:page => params[:page])
+      end
+    end
+  end
+      #:fields => [:title, :user_id, :score, :comments_count, :is_blocked, :created_at],
+      #:associations => { :belongs_to => { :user => :user_id, :source => :source } },
+
+  def show
   end
 
   def new
     @content = Content.new
   end
 
+  def create
+    @content = Content.new(params[:content])
+    @content
+    if @content.save
+      flash[:success] = "Successfully created your new Content!"
+      redirect_to admin_content_path(@content)
+    else
+      flash[:error] = "Could not create your Content, please try again"
+      render :action => :new
+    end
+  end
+
   def edit
-    @content = Content.find(params[:id])
-# todo: not completed
-#    render_edit @content
   end
 
   def update
-    @content = Content.find(params[:id])
     if @content.update_attributes(params[:content])
       flash[:success] = "Successfully updated your Content."
-      @content.expire
-      redirect_to [:admin, @content]
+      redirect_to admin_content_path(@content)
     else
       flash[:error] = "Could not update your Content as requested. Please try again."
-      render :edit
+      render :action => :edit
     end
   end
 
-  def show
-    render 'shared/admin/show_page', :layout => 'new_admin', :locals => {
-      :item => Content.find(params[:id]),
-      :model => Content,
-      :fields => [:title, :user_id, :url, :caption, :content_image, :source, :score, :comments_count, :is_blocked, :created_at],
-      :associations => { :belongs_to => { :user => :user_id , :source => :source}, :has_one => { :content_image => :content_image} },
-    }
-  end
+  # Show
+  #:fields => [:title, :user_id, :url, :caption, :content_image, :source, :score, :comments_count, :is_blocked, :created_at],
+  # :associations => { :belongs_to => { :user => :user_id , :source => :source}, :has_one => { :content_image => :content_image} },
 
-  def create
-    @content = Content.new(params[:content])
-    @content.user = current_user
-    # TODO: Check this code because I think that story is not used anymore for Content
-    #@story.tag_list = params[:content][:tags_string]
-    #if params[:content][:image_url].present?
-    #  @story.build_content_image({:url => params[:content][:image_url]})
-    #end
-    if @content.save
-      flash[:success] = "Successfully created your new Content!"
-      redirect_to [:admin, @content]
-    else
-      flash[:error] = "Could not create your Content, please try again"
-      render :new
-    end
-  end
-
-  def render_edit content
-    render 'shared/admin/edit_page', :layout => 'new_admin', :locals => {
-      :item => content,
-      :model => Content,
-      :fields => [:title, :user_id, :url, :caption, :source, :score, :comments_count, :is_blocked, :created_at],
-      :associations => { :belongs_to => { :user => :user_id , :source => :source}, :has_one => { :content_image => :content_image} },
-    }
-  end
+  # Edit
+  #:fields => [:title, :user_id, :url, :caption, :source, :score, :comments_count, :is_blocked, :created_at],
+  #:associations => { :belongs_to => { :user => :user_id , :source => :source}, :has_one => { :content_image => :content_image} },
 
   def destroy
-    @content = Content.find(params[:id])
-    @content.destroy
+    if @content.destroy
+      flash[:success] = "Successfully deleted your Content."
+    else
+      flash[:error] = "Could not delete your Content as requested. Please try again."
+    end
 
-    redirect_to admin_contents_path
+    redirect_to :action => :index
   end
 
   private
 
-  def set_current_tab
-    @current_tab = 'contents';
+  def find_content
+    @content = Content.find(params[:id])
   end
 
 end
